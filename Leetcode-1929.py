09072ee738bbe221cc94da3203def3d7413b5611
Sirr003 <sirrseals17@gmail.com>
2026-07-11 13:28:30 -0500

    Create Leetcode-1929.py

---
+++ Leetcode-1929.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(len(nums) * 2):
+            ans.append(nums[i % len(nums)])
+        return ans
